5c2fadc376f19a5b89de78149fae5436092ed090
hussain-shariff <hussainowaiz070@gmail.com>
2023-07-05 18:11:08 +0530

    button story

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,39 +1,35 @@
 import { Button } from './Button';
 
-// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 export default {
   title: 'Example/Button',
   component: Button,
-  tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 };
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Primary = {
   args: {
-    primary: true,
+    variant : 'primary',
     label: 'Button',
   },
 };
 
 export const Secondary = {
   args: {
+    variant : 'secondary',
     label: 'Button',
   },
 };
 
-export const Large = {
+export const Success = {
   args: {
-    size: 'large',
+    variant : 'success',
     label: 'Button',
   },
 };
 
-export const Small = {
+export const Error = {
   args: {
-    size: 'small',
+    variant : 'error',
     label: 'Button',
   },
 };
